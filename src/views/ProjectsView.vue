66b680f4a1aa4375db2657d0da1b2b6027871ec4
Adrián Sanz <adriybeto@hotmail.com>
2023-09-12 16:54:36 +0200

    Dark mode projects dashboard

--- src/views/ProjectsView.vue
+++ src/views/ProjectsView.vue
@@ -42,7 +42,6 @@ function getProjects() {
     })
     .then(response => {
         projects.value = response.data;
-        console.log(response.data);
     })
     .catch(error => {
         console.log(error);
@@ -52,20 +51,29 @@ function getProjects() {
 // Tests warnings
 const { showGlobalWarning } = inject('warning');
 
+// Toggle Project view
+function toggleProjectView(event) {
+    const index = event.target.dataset.index;
+    const projects = document.querySelectorAll(".project");
+    const targeted = projects[index];
+    targeted.classList.toggle("project-open");
+}
+
 // Options 
 function activeOptions(event) {
     const index = event.target.dataset.index;
     const allOptions = document.querySelectorAll(".project-body-options-menu");
     const targeted = allOptions[index];
+    event.target.classList.toggle("project-body-options-menu-active-button");
     targeted.classList.toggle("project-body-options-menu-active");
 }
 
 </script>
 <template>
-    <p>{{user}}</p>
-    <button style="margin: .4rem 0; width: 10rem; align-self: center;" @click="showGlobalWarning('success', 'Invitación enviada correctamente.')">Enviado correctamente</button>
+    <!-- <p>{{user}}</p> -->
+    <!-- <button style="margin: .4rem 0; width: 10rem; align-self: center;" @click="showGlobalWarning('success', 'Invitación enviada correctamente.')">Enviado correctamente</button>
     <button style="margin: .4rem 0; width: 10rem; align-self: center;" @click="showGlobalWarning('warn', 'El usuario ya pertenece a tu proyecto.')">Aviso</button>
-    <button style="margin: .4rem 0; width: 10rem; align-self: center;" @click="showGlobalWarning('error', 'Ha ocurrido un error y no se ha podido enviar la invitación.')">Error al enviar</button>
+    <button style="margin: .4rem 0; width: 10rem; align-self: center;" @click="showGlobalWarning('error', 'Ha ocurrido un error y no se ha podido enviar la invitación.')">Error al enviar</button> -->
     <main>
         <div class="head">
             <h1>{{ translations?.title?.[lang] }}</h1>
@@ -83,9 +91,9 @@ function activeOptions(event) {
                         <a href="#">
                             <img src="/icons/external.svg" alt="External link Icon">
                         </a>
-                        <h2>{{ project.title }}</h2>
-                        <button :data-index="index">
-                            <img src="/icons/arrow.svg" alt="Arrow Icon">
+                        <h2 :data-index="index" @click="toggleProjectView($event)">{{ project.title }}</h2>
+                        <button :data-index="index" @click="toggleProjectView($event)">
+                            <img :data-index="index" src="/icons/arrow.svg" alt="Arrow Icon">
                         </button>
                     </div>
 
@@ -132,8 +140,11 @@ function activeOptions(event) {
                             </div>
                         </div>
                         <div class="project-body-options">
-                            <button @click="activeOptions($event)">
-                                <img :data-index="index" width="24" height="24" src="/icons/dots.svg" alt="Dots Icon for options">
+                            <button :data-index="index" @click="activeOptions($event)">
+                                <span></span>
+                                <span></span>
+                                <span></span>
+                                <!-- <img :data-index="index" width="24" height="24" src="/icons/dots.svg" alt="Dots Icon for options"> -->
                             </button>
                             <div class="project-body-options-menu">
                                 <ul>
@@ -176,9 +187,31 @@ h1 {
     font-weight: bold;
 }
 
+html[data-theme=dark] h1 {
+    color: var(--lightWhite);
+}
+
 .body {
     margin: 0 auto;
     width: 90%;
+}
+
+@media screen and (min-width: 600px) {
+    .body {
+        width: 80%;
+    }
+}
+
+@media screen and (min-width: 900px) {
+    .body {
+        width: 70%;
+    }
+}
+
+@media screen and (min-width: 1200px) {
+    .body {
+        width: 50%;
+    }
 }
 
 .body > a button {
@@ -191,6 +224,12 @@ h1 {
     font-size: 1rem;
     border: 1px solid var(--darkBlueText);
     cursor: pointer;
+}
+
+html[data-theme=dark] .body > a button {
+    background-color: var(--darkBlueText);
+    color: var(--lightWhite);
+    border-color: var(--darkBlueHF);
 }
 
 /* Projects */
@@ -206,7 +245,13 @@ h1 {
     align-items: center;
     background-color: #EF7264;
     padding: .5rem 1rem;
-    border-radius: 5px 5px 0 0;
+    border-radius: 5px;
+    transition: all .5s;
+    gap: 2rem;
+}
+
+html[data-theme=dark] .project-head {
+    background-color: var(--darkBlue);
 }
 
 .project-head a {
@@ -226,23 +271,62 @@ h1 {
     transform: translateY(-50%);
 }
 
+html[data-theme=dark] .project-head a img {
+    filter: invert(1);
+}
+
+html[data-theme=dark] .project-head a::after {
+    background-color: rgba(128, 128, 128, 0.5);
+}
+
 .project-head h2 {
     font-size: 1rem;
     color: var(--darkBlueText);
+    width: 100%;
+    text-align: center;
+}
+
+html[data-theme=dark] .project-head h2 {
+    color: var(--lightWhite);
 }
 
 .project-head button {
     background-color: transparent;
     border: none;
+    cursor: pointer;
+}
+
+html[data-theme=dark] .project-head button {
+    filter: invert(1);
+}
+
+.project-open .project-head {
+    border-radius: 5px 5px 0 0;
+}
+
+.project-open .project-body {
+    max-height: 50rem;
+    padding: .5rem 1rem;
+}
+
+.project-open .project-head button img {
+    transform: rotate(180deg);
 }
 
 .project-body {
     display: flex;
     justify-content: space-between;
     background-color: #F48F83;
-    padding: .5rem 1rem;
     border-radius: 0 0 5px 5px;
     position: relative;
+    max-height: 0rem;
+    overflow: hidden;
+    transition: all .45s;
+    padding: 0rem 1rem;
+}
+
+html[data-theme=dark] .project-body {
+    background-color: var(--darkBlueHF);
 }
 
 .project-body > * {
@@ -259,9 +343,17 @@ h1 {
     color: var(--darkBlueText);
 }
 
+html[data-theme=dark] .project-body h3 {
+    color: var(--lightWhite);
+}
+
 .project-body-state-nums p {
     font-size: .8rem;
     color: var(--darkBlueText);
+}
+
+html[data-theme=dark] .project-body-state-nums p {
+    color: var(--lightWhite);
 }
 
 .project-body-members-holder {
@@ -299,6 +391,10 @@ h1 {
     color: var(--darkBlueText);
 }
 
+html[data-theme=dark] .project-body-members-member span {
+    background-color: var(--lightWhite);
+}
+
 .project-body-members-member img {
     border-radius: 100%;
     width: 28px;
@@ -306,6 +402,10 @@ h1 {
     border: 1px solid var(--darkBlueText);
     object-fit: cover;
     transition: all .25s;
+}
+
+html[data-theme=dark] .project-body-members-member img {
+    border-color: rgba(128, 128, 128, 0.5);
 }
 
 .project-body-members-member:hover img {
@@ -323,21 +423,31 @@ h1 {
     padding: 0;
     display: flex;
     cursor: pointer;
+    gap: 0.25rem;
+    transition: all .5s;
+}
+
+.project-body-options button span {
+    transition: all .15s;
+    font-size: 2rem;
+    background-color: #d03928;
+    width: 6px;
+    height: 6px;
+    border-radius: 100vw;
+    pointer-events: none;
 }
 
 .project-body-options-menu {
     overflow: hidden;
-    transition: all 1s;
+    transition: all .75s;
     max-height: 30rem;
-    position: absolute;
-    z-index: 99;
     top: 50%;
-    transform: translateY(-50%);
+    visibility: hidden;
 }
 
 .project-body-options-menu-active {
-    /* height: 10rem; */
     opacity: 1;
+    visibility: visible;
 }
 
 .project-body-options-menu ul {
@@ -346,10 +456,9 @@ h1 {
     flex-direction: column;
     gap: .25rem;
     padding: 0rem;
-    height: 0;
+    height: 0px;
     overflow: hidden;
-    transition: all 1s;
-    transition: height 0.5s ease;
+    transition: all .75s ease;
 }
 
 .project-body-options-menu-active ul {
@@ -363,6 +472,10 @@ h1 {
     padding: .4rem;
     border: 1px solid rgba(60, 59, 59, 0.208);
 }
+
+html[data-theme=dark] .project-body-options-menu ul li {
+    border-color: rgba(128, 128, 128, 0.5);
+}
 .project-body-options-menu ul li button {
     text-align: center;
     padding: .1rem;
@@ -370,10 +483,17 @@ h1 {
     font-size: .7rem;
     cursor: pointer;
 }
+.project-body-options-menu-active-button {
+    gap: 1rem !important;
+}
 
 .project-body-options-menu-general {
     background-color: #dd6153;
     transition: all .25s;
+}
+
+html[data-theme=dark] .project-body-options-menu-general {
+    background-color: var(--darkBlue);
 }
 
 .project-body-options-menu-general button {
@@ -382,8 +502,16 @@ h1 {
     transition: all .25s;
 }
 
+html[data-theme=dark] .project-body-options-menu-general button {
+    color: var(--lightWhite);
+}
+
 .project-body-options-menu-general:hover {
     background-color: #d54434;
+}
+
+html[data-theme=dark] .project-body-options-menu-general:hover {
+    background-color: rgb(48, 91, 116);;
 }
 
 
@@ -400,6 +528,16 @@ h1 {
 .project-body-options-menu-remove:hover {
     background-color: rgb(44, 44, 57)}
 
-.project-body-options-menu-remove:hover button {
+html[data-theme=dark] .project-body-options-menu-remove {
+    background-color: var(--lightRed);
 }
+
+html[data-theme=dark] .project-body-options-menu-remove button {
+    color: var(--darkBlueHF);
+}
+
+html[data-theme=dark] .project-body-options-menu-remove:hover {
+    background-color: #e7311c;
+}
+
 </style>
